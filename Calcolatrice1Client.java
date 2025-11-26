bf4dd96ae35d7544c3c4984bb20ffe47b585a179
megamazzz <mazzei.christian@itiscannizzaro.edu.it>
2025-11-26 23:28:23 +0100

    Create Calcolatrice1Client.java

---
+++ Calcolatrice1Client.java
@@ -0,0 +1,41 @@
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class CalcolatriceClient {
+    private static final String HOST = "localhost";
+    private static final int PORTA = 8844;
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(HOST, PORTA);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(socket.getInputStream()));
+             Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Connesso al server " + HOST + ":" + PORTA);
+
+            System.out.println("=== CALCOLATRICE REMOTA ===");
+            System.out.println("Formato: NUMERO OPERAZIONE NUMERO");
+            System.out.println("Operazioni: + - * /");
+            System.out.println("Scrivi 'quit' per uscire");
+
+            while (true) {
+                System.out.print("Calcolo > ");
+                String input = scanner.nextLine();
+
+                if ("quit".equalsIgnoreCase(input)) break;
+
+                out.println(input);
+                String risposta = in.readLine();
+                System.out.println("Server: " + risposta);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Errore client: " + e.getMessage());
+        }
+
+        System.out.println("Client disconnesso.");
+    }
+}
